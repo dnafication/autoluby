fb9f2f368fa38a07298bb8c38afad774dd57e097
Paulo - CNT <pa.aabreu18@gmail.com>
2021-12-18 11:13:31 -0300

    inital config

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const routes = require('./routes')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(process.env.PORT, () => console.log('Server is running'))
